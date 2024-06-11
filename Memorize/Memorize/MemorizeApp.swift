7d590fd910a208cefec2c79d1be44bfe171b7654
chanoktrue <chanoktrue@me.com>
2024-06-11 09:32:11 +0700

    11.06.2024

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Thongchai Subsaidee on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
