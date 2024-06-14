3c99f65d841b3510a2ff2a7f9483fcb6754b5890
chanoktrue <chanoktrue@me.com>
2024-06-15 06:08:11 +0700

    15.06.2024

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻", "🎃", "🕷️", "😈"]
     var body: some View {
         HStack{
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            CardView(content: emojis[0], isFaceUp: true)
+            CardView(content: emojis[1], isFaceUp: false)
+            CardView(content: emojis[2], isFaceUp: false)
+            CardView(content: emojis[3], isFaceUp: false)
         }
         .foregroundColor(.orange)
         .imageScale(.small)
@@ -22,14 +23,15 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    @State var isFaceUp: Bool = false
+    var content: String
+    @State var isFaceUp = false
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }else{
                 RoundedRectangle(cornerRadius: 12)
             }
